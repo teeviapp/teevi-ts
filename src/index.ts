baee191269c3ba1b2f18ce01fd61dc5135631ce6
manueldidonna <manueldidonnadev@gmail.com>
2024-09-12 14:12:04 +0200

    chore: remove fetch declaration & Source type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-export * from "./source"
-export * from "./network"
+export type ShowKind = "movie" | "series"
+
+export type SearchShow = {
+  id: string
+  kind: ShowKind
+  title: string
+  posterURL?: string
+}
+
+export type Show = SearchShow & {
+  backdropURL?: string
+  overview: string
+  durationInSeconds: number
+  genres: string[]
+  seasons?: { first: number; last: number }
+}
+
+export type ShowEpisode = {
+  id: string
+  number: number
+  title?: string
+  thumbnailURL?: string
+  overview?: string
+  durationInSeconds?: number
+}
+
+export type SearchShowsFuntion = (query: string) => Promise<SearchShow[]>
+export type FetchShowDetailsFuntion = (id: string) => Promise<Show>
+export type FetchEpisodesFuntion = (showId: string) => Promise<ShowEpisode[]>
