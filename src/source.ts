3771d26b68e4bfec87ecdfe5394e87d1cb1e9bbd
manueldidonna <manueldidonnadev@gmail.com>
2024-09-12 10:48:53 +0200

    feat: add fetch function fallback due to lack of DOM in execution environment

---
+++ src/source.ts
@@ -0,0 +1,35 @@
+export type ShowKind = "movie" | "series"
+
+export type SearchShow = {
+  id: string
+  kind: ShowKind
+  title: string
+  posterURL?: string
+}
+
+export type Show = SearchShow & {
+  backdropURL?: string
+  overview: string
+  durationInSeconds: number
+  genres: string[]
+  seasons?: { first: number; last: number }
+}
+
+export type ShowEpisode = {
+  id: string
+  number: number
+  title?: string
+  thumbnailURL?: string
+  overview?: string
+  durationInSeconds?: number
+}
+
+export type SearchShowsFuntion = (query: string) => Promise<SearchShow[]>
+export type FetchShowDetailsFuntion = (id: string) => Promise<Show>
+export type FetchEpisodesFuntion = (showId: string) => Promise<ShowEpisode[]>
+
+export type Source = {
+  searchShows: SearchShowsFuntion
+  fetchShowDetails: FetchShowDetailsFuntion
+  fetchEpisodes: FetchEpisodesFuntion
+}
