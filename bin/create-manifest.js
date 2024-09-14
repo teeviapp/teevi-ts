0ccb9a8d79093bb0905781a02640d0c2ff656167
manueldidonna <manueldidonnadev@gmail.com>
2024-09-14 16:35:20 +0200

    update create-manifest script

--- bin/create-manifest.js
+++ bin/create-manifest.js
@@ -1,41 +1,39 @@
 #!/usr/bin/env node
-import { readFile, writeFile } from "fs"
-import { join } from "path"
+import { readFile, writeFile } from "fs/promises"
+import { join, dirname } from "path"
 
-const packageJsonPath = join(process.cwd(), "package.json")
-const manifestPath = join(process.cwd(), "manifest.json")
-
-// Read the package.json file
-readFile(packageJsonPath, "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading package.json:", err)
-    process.exit(1)
-  }
-
+const getPackageJson = async (path) => {
   try {
-    const packageJson = JSON.parse(data)
-
-    // Define the content for manifest.json using information from package.json
-    const manifestContent = {
-      name: packageJson.name,
-      version: packageJson.version,
-      description: packageJson.description || "Thirt-party source for Teevi",
-      author: packageJson.author || "Unknown",
-    }
-
-    // Write the manifest.json file
-    writeFile(manifestPath, JSON.stringify(manifestContent, null, 2), (err) => {
-      if (err) {
-        console.error("Error writing manifest.json:", err)
-        process.exit(1)
-      }
-
-      console.log(
-        "manifest.json has been created successfully using package.json data!"
-      )
-    })
-  } catch (parseError) {
-    console.error("Error parsing package.json:", parseError)
+    const json = await readFile(join(path, "package.json"), "utf8")
+    return JSON.parse(json)
+  } catch (error) {
+    console.error(`Error reading ${path}/package.json:`, error)
     process.exit(1)
   }
-})
+}
+
+const libraryPackage = await getPackageJson(dirname(import.meta.dirname))
+const sourcePackage = await getPackageJson(process.cwd())
+
+if (!sourcePackage.versionCode) {
+  console.error("Error missing versionCode from package.json")
+  process.exit(1)
+}
+
+// Define the content for manifest.json using information from package.json
+const manifestContent = {
+  name: sourcePackage.name,
+  versionCode: sourcePackage.versionCode,
+  libraryVersion: libraryPackage.version,
+  description: sourcePackage.description || "Thirt-party source for Teevi",
+  author: sourcePackage.author || "Unknown",
+}
+
+try {
+  const manifestPath = join(process.cwd(), "manifest.json")
+  await writeFile(manifestPath, JSON.stringify(manifestContent, null, 2))
+  console.log("manifest.json has been created successfully!")
+} catch (err) {
+  console.error("Error writing manifest.json:", err)
+  process.exit(1)
+}
